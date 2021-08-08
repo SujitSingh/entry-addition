9ba8c97c9f7ef89734984e88931dee53c4ed4563
Sujit Kumar Singh <sujit98322@gmail.com>
2021-08-09 01:26:33 +0530

    name and path changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Wearsafe-labs'),
+      dir: require('path').join(__dirname, './coverage/input-entries'),
       subdir: '.',
       reporters: [
         { type: 'html' },
